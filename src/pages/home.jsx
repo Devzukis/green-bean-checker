81079cccc56fe877b390ab1845381041211237e4
Jade Graham <total.trance.network@gmail.com>
2023-07-03 17:25:12 +0100

    Remove old refs

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,11 +5,9 @@ import Layout from "../common/layout";
 import Footer from "../components/section/footer/";
 import About from "../components/section/about/";
 import MetamaskModal from "../common/modal/metamaskModal/MetamaskModal";
-import MintNowModal from "../common/modal/mintNowModal";
 import WalletModal from "../common/modal/walletModal/WalletModal";
 const HomeV1 = () => {
   const {
-    visibility,
     metamaskModal,
     walletModalvisibility
   } = useModal();
@@ -17,7 +15,6 @@ const HomeV1 = () => {
     <Layout>
       <GlobalStyles />
       {metamaskModal && <MetamaskModal />}
-      {visibility && <MintNowModal />}
       {walletModalvisibility && <WalletModal />}
       <Header />  
       <About /> 
