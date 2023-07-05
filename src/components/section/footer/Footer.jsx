b461f877fda398dfddcde9645d24a84cc74175c1
Alfonso <alfonso@viruett.com>
2023-07-05 13:33:51 -0700

    updates footer

--- src/components/section/footer/Footer.jsx
+++ src/components/section/footer/Footer.jsx
@@ -1,37 +1,10 @@
-import backToTopIcon from "../../../assets/images/icon/back_to_top.svg";
-import footerShapesLeft from "../../../assets/images/icon/footer_shapes_left.png";
-import footerShapesRight from "../../../assets/images/icon/footer_shapes_right.png";
 import FooterStyleWrapper from "./Footer.style";
+import { FaDiscord, FaGithub, FaTwitter } from "react-icons/fa";
 const Footer = () => {
   return (
-    <FooterStyleWrapper>
-
-      <div className="bithu_v1_main_footer">
-
-        <div className="footer_bottom">
-          <div className="footer_bottom_content">
-            <span className="footer_shapes_left">
-              <img src={footerShapesLeft} alt="devzukis nft footer" />
-            </span>
-            <span className="footer_shapes_right">
-              <img src={footerShapesRight} alt="devzukis nft footer" />
-            </span>
-            <div className="container">
-              <div className="footer_menu">
-                <div className="bottom_footer_left">
-                  <div className="copiright_text">
-                    <p>Copyright © 2023 Devzukis</p>
-                  </div>
-                </div>
-                <a href="# " className="bact_to_top_btn">
-                  <img src={backToTopIcon} alt="devzukis nft back to top" />
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </FooterStyleWrapper>
+    <div className="flex justify-center p-10">
+      <p className='text-black'>Copyright © 2023 Devzukis</p>
+    </div>
   );
 };
 
