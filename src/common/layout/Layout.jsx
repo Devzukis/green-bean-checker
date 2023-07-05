635cea209b8119bbe309999425174619f26e31b7
Alfonso <alfonso@viruett.com>
2023-07-05 13:35:21 -0700

    updates tab title

--- src/common/layout/Layout.jsx
+++ src/common/layout/Layout.jsx
@@ -8,7 +8,7 @@ const Layout = ({ children }) => {
         <meta charset="utf-8" />
         <title>
           {" "}
-          Devzukis - NFT Minting/Collection Landing Page React-Nextjs Template
+          Devzukis - Green Bean Checker
         </title>
         <meta name="description" content="" />
         <link rel="shortcut icon" type="image/x-icon" href={favIcon} />
