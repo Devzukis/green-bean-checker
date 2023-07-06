70fcc4e6064c186657b2d44053151b8ffcb780af
Alfonso <alfonso@viruett.com>
2023-07-05 19:34:43 -0700

    show green bean on load error

--- src/components/section/Card.js
+++ src/components/section/Card.js
@@ -1,10 +1,17 @@
+import { useState } from 'react';
+import greenbean from '../../assets/images/greenBean.webp';
 
 const Card = (props) => {
     const { src, title } = props;
+    const [source, setSource] = useState(src);
+
+    const onImageLoadError = () => {
+        setSource(greenbean);
+    }
 
     return (
         <div className='flex flex-col bg-white gap-2 sm:gap-4 p-2 sm:p-4 rounded-lg'>
-            <img className='rounded-lg' src={src}/>
+            <img className='rounded-lg' src={source} onError={onImageLoadError}/>
             <p className='text-sm sm:text-lg text-black font-bold'>{title}</p>
         </div>
     )
