0b37da7b2c3847b7bd3b2a9e293d54339a661e27
Alfonso <alfonso@viruett.com>
2023-07-06 12:21:43 -0700

    adds vercel analytics

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,15 +3,19 @@ import Header from "../components/section/header/desktop/Header";
 import Layout from "../common/layout";
 import Footer from "../components/section/footer/Footer";
 import About from "../components/section/about/About";
+import { Analytics } from '@vercel/analytics/react';
 
 const HomeV1 = () => {
   return (
+    <>
     <Layout>
       <GlobalStyles />
       <Header />  
       <About /> 
       <Footer />
     </Layout>
+    <Analytics/>
+    </>
   );
 };
 
